ce33226f4242bd451cd16d606dce1cf831db911b
RitamCrafts <ritamrm1157@gmail.com>
2026-01-21 18:35:13 +0530

    JS connect all IDs recieve

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("JS active!");
+
+//TIME TO GET ALL ID
+const searchButtonE=document.getElementById("search-button");
+const cityNameDisplayE=document.getElementById("city-name-display");
+const countryNameDisplayE=document.getElementById("country-name-display");
+const locationDisplayE=document.getElementById("location-display");
+const weatherIconE=document.getElementById("weather-icon");
+const currentStatusE=document.getElementById("current-status");
+const currentTempE=document.getElementById("current-temp");
+const unitE=document.getElementById("unit");
